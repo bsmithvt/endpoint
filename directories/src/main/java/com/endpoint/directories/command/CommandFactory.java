134a370e59a5d25c24ed0553d86f862704c94bd0
bsmithvt <bsmithvt@users.noreply.github.com>
2024-09-17 11:53:14 -0400

    make CommandFactory package local scope

--- directories/src/main/java/com/endpoint/directories/command/CommandFactory.java
+++ directories/src/main/java/com/endpoint/directories/command/CommandFactory.java
@@ -1,12 +1,12 @@
 package com.endpoint.directories.command;
 
-public class CommandFactory {
+class CommandFactory {
     private static final String CREATE = "create";
     private static final String MOVE = "move";
     private static final String DELETE = "delete";
     private static final String LIST = "list";
 
-    public static Command createCommand(String command) {
+    static Command createCommand(String command) {
         if (command == null) throw new NullPointerException("command cannot be null.");
         return switch (command.toLowerCase().trim()) {
             case CREATE -> new Create();
